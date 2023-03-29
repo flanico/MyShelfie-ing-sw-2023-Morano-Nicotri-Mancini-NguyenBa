a933644b59bee1f6ab0c0c074721333f8cd6db99
alemancio5 <alemancio01@icloud.com>
2023-03-29 19:07:39 +0200

    Inserimento di Javadoc in Tile e Player

--- MyShelfie/src/main/java/it/polimi/ingsw/Player.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Player.java
@@ -10,15 +10,28 @@ public class Player {
     private final Bookshelf bookshelf;
     private PersonalGoalCard personalgoalcard;
 
+    /**
+     * constructor of Player
+     * @param nickname of the player
+     * @author Alessandro Mancini
+     */
     public Player(String nickname) {
         this.nickname = nickname;
         this.bookshelf = new Bookshelf();
     }
 
+    /**
+     * getter of nickname
+     * @author Alessandro Mancini
+     */
     public String getNickname() {
         return this.nickname;
     }
 
+    /**
+     * getter of bookshelf
+     * @author Alessandro Mancini
+     */
     public Bookshelf getBookshelf() {
         return bookshelf;
     }
--- MyShelfie/src/main/java/it/polimi/ingsw/Tile.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Tile.java
@@ -12,6 +12,11 @@ public class Tile {
     private boolean blocked;
     private boolean counted;
 
+    /**
+     * constructor of Tile
+     * @param type of the tile
+     * @author Alessandro Mancini
+     */
     public Tile(TileType type) {
         this.type = type;
         this.x = -1;
@@ -19,6 +24,13 @@ public class Tile {
         this.blocked = false;
     }
 
+    /**
+     * constructor of Tile
+     * @param type of the tile
+     * @param x coordinate of the tile
+     * @param y coordinate of the tile
+     * @author Alessandro Mancini
+     */
     public Tile(TileType type, int x, int y) {
         this.type = type;
         this.x = x;
@@ -26,42 +38,87 @@ public class Tile {
         this.blocked = false;
     }
 
+    /**
+     * getter of type
+     * @author Alessandro Mancini
+     */
     public TileType getType() {
         return type;
     }
 
+    /**
+     * setter of type
+     * @param type
+     * @author Alessandro Mancini
+     */
     public void setType(TileType type) {
         this.type = type;
     }
 
+    /**
+     * getter of x
+     * @author Alessandro Mancini
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * setter of x
+     * @param x
+     * @author Alessandro Mancini
+     */
     public void setX(int x) {
         this.x = x;
     }
 
+    /**
+     * getter of y
+     * @author Alessandro Mancini
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * setter of y
+     * @param y
+     * @author Alessandro Mancini
+     */
     public void setY(int y) {
         this.y = y;
     }
 
+    /**
+     * getter of blocked
+     * @author Alessandro Mancini
+     */
     public boolean isBlocked() {
         return blocked;
     }
 
+    /**
+     * setter of blocked
+     * @param blocked
+     * @author Alessandro Mancini
+     */
     public void setBlocked(boolean blocked) {
         this.blocked = blocked;
     }
 
+    /**
+     * getter of counted
+     * @author Alessandro Mancini
+     */
     public boolean isCounted() {
         return counted;
     }
 
+    /**
+     * setter of counted
+     * @param counted
+     * @author Alessandro Mancini
+     */
     public void setCounted(boolean counted) {
         this.counted = counted;
     }
