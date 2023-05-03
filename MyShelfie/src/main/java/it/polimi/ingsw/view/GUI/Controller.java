931c405d4d19d4575f06659e11f04366a8b33f91
stefano-morano <stefano.morano@mail.polimi.it>
2023-05-03 17:33:03 +0200

    Creato il menu della GUI

--- MyShelfie/src/main/java/it/polimi/ingsw/view/GUI/Controller.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/view/GUI/Controller.java
@@ -2,15 +2,29 @@ package it.polimi.ingsw.view.GUI;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javax.swing.text.html.ImageView;
 
 public class Controller {
     @FXML
+    TextField serverPortField ;
+    @FXML
+    TextField serverAddressField;
+    @FXML
+    TextField usernameField;
+    Alert alert = new Alert(Alert.AlertType.ERROR);
+    public void joinGame(ActionEvent actionEvent) {
 
-    Label scritta;
 
-    public void showtitle(ActionEvent actionEvent){
-        scritta.setOpacity(100);
+        if ( usernameField.getText().compareTo("")==0){
+            alert.setTitle("Errore");
+            alert.setHeaderText(null);
+            alert.setContentText("Inserisci username");
+            alert.showAndWait();
+        }
+
     }
-
 }
