d60804782232b7a7f997514db90aaecd5cf0c8bf
alemancio5 <alemancio01@icloud.com>
2023-05-13 10:47:19 +0200

    RMIServer fix

--- MyShelfie/src/main/java/it/polimi/ingsw/network/server/RMIServer.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/network/server/RMIServer.java
@@ -1,13 +1,14 @@
 package it.polimi.ingsw.network.server;
 
+import it.polimi.ingsw.network.RemoteInterface;
+
 import java.rmi.AlreadyBoundException;
-import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
-public class RMIServer extends UnicastRemoteObject implements Runnable {
+public class RMIServer extends UnicastRemoteObject implements RemoteInterface, Runnable {
     protected final Server server;
     private final int port;
 
@@ -16,16 +17,18 @@ public class RMIServer extends UnicastRemoteObject implements Runnable {
         this.server = server;
     }
 
+    public String sayHello() throws RemoteException{
+        return ("HELLO");
+    }
+
     @Override
     public void run() {
         try {
             Registry registry = LocateRegistry.createRegistry(this.port);
-            registry.bind("server", (Remote)this);
+            registry.bind("rmi server", this);
             System.out.println("Server is ready");
         } catch (RemoteException | AlreadyBoundException e) {
             throw new RuntimeException(e);
         }
-
-
     }
 }
