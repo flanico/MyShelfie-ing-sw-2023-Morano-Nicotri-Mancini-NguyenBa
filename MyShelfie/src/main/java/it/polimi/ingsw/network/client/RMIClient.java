3a4ec94545c991c2e5ab99d3005ac180b59ef71a
alemancio5 <alemancio01@icloud.com>
2023-05-13 16:04:31 +0200

    fix RMIClient

--- MyShelfie/src/main/java/it/polimi/ingsw/network/client/RMIClient.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/network/client/RMIClient.java
@@ -1,20 +1,20 @@
 package it.polimi.ingsw.network.client;
 
 import it.polimi.ingsw.network.message.Message;
-import it.polimi.ingsw.network.server.RMIImplementation;
 
 import java.rmi.NotBoundException;
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class RMIClient extends Client {
     private final Registry registry;
-    private final RMIImplementation remote;
+    private final Remote remote;
 
     public RMIClient(String ip, int port) throws RemoteException, NotBoundException {
         this.registry = LocateRegistry.getRegistry(ip, port);
-        this.remote = (RMIImplementation) this.registry.lookup("rmiServer");
+        this.remote = this.registry.lookup("rmiServer");
     }
 
     public void sendMessage(Message message) {}
