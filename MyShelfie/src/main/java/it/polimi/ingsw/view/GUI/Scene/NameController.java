693c720be205dc048f054e11c79f3c003c2fab65
stefano-morano <stefano.morano@mail.polimi.it>
2023-05-17 16:34:24 +0200

    new graphics

--- MyShelfie/src/main/java/it/polimi/ingsw/view/GUI/Scene/NameController.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/view/GUI/Scene/NameController.java
@@ -5,11 +5,16 @@ import it.polimi.ingsw.view.GUI.SceneController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class NameController extends ViewObservable implements Controller {
     @FXML
     TextField usernameField;
-    public void continuePressed(ActionEvent actionEvent) {
+    @FXML
+    ImageView continueButton;
+
+    public void continueButtonPressed(MouseEvent mouseEvent) {
         String Nickname = usernameField.getText();
         if (Nickname.compareTo("")==0)
             SceneController.showAlert("The nickname field is empty!");
